3bbb87f7e54cb85c2ec0cba3b721680f8cb99c48
Tom Harwood <tharwood26@gmail.com>
2020-03-30 11:54:33 +0100

    chatroom now working

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import { logger } from 'redux-logger';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
+import messagesReducer from './reducers/messages_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
+
+const identityReducer = (state = null) => state;
+
+// State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  messages: messagesReducer,
+  channels: identityReducer,
+  currentUser: identityReducer,
+  selectedChannel: selectedChannelReducer
 });
+
+const initialState = {
+  messages: [],
+  channels: ['general', 'react', 'paris'],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
+// middlwares
+const middlewares = applyMiddleware(logger, reduxPromise);
+const store = createStore(reducers, initialState, middlewares);
+
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
